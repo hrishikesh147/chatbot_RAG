2b93844304388d5740de2266cd593de37b917792
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2025-02-03 12:16:52 +0530

    foldere striucture updated

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages,setup
+
+setup(name="RAG Application",
+      version="0.0.0.0",
+      description="Rag approach",
+      packages=find_packages(),
+      install_requires=[])
